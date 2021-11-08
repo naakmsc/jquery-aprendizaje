d7499ad86ec7f73879f835123151844e2c25b947
Naakmsc <isaigutierrez125@gmail.com>
2021-11-08 00:17:15 -0600

    Aprendiendo Ajax con Jequery Get y Post

---
+++ ajax/archivo.php
@@ -0,0 +1,7 @@
+<div id="contenido">
+    <h1 style="background:red; color:white;">
+        Este es un texto traido del servidor con php
+    </h1>
+</div>
+
+<div id="ninguno">Este es otro contenido</div>
